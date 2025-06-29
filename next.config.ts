def0b806fa9266e1d29ef5888a61158532105bbe
Lowkey Dev <174181680+lowkey-dev21@users.noreply.github.com>
2025-06-29 23:29:22 +0100

    chore: deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: "dist",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/chunks/[name].[hash][ext]",
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
